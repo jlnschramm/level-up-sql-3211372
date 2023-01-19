7df28ede3eca94dfed62bd58157ff542259db653
Julian Schramm <63654688+jlnschramm@users.noreply.github.com>
2023-01-19 16:55:00 +0000

    up to 07_01

--- 01_restaurant/challenges/01_06_removeinfo_ch.sql
+++ 01_restaurant/challenges/01_06_removeinfo_ch.sql
@@ -1,7 +1,11 @@
 -- Remove incorrect information from the database.
 SELECT CustomerID FROM Customers WHERE FirstName='Norby'
 SELECT * FROM Reservations WHERE CustomerID=64
-DELETE FROM Reservations WHERE
+SELECT Customers.FirstName, Reservations.Date
+DELETE FROM Reservations AS
+JOIN Customers ON Reservations.CustomerID=Customers.CustomerID
+WHERE Customers.FirstName='Norby' AND Reservations.Date>='2022-07-24'
+DELETE FROM Reservations WHERE 
 -- A customer named Norby has notified us he won't 
 -- be able to keep his Friday reservation. 
 -- Today is July 24, 2022.
--- 01_restaurant/challenges/01_07_attendevent_ch.sql
+++ 01_restaurant/challenges/01_07_attendevent_ch.sql
@@ -2,3 +2,6 @@
 
 -- The customer 'atapley2j@kinetecoinc.com' will be in
 -- attendance, and will bring 3 friends.
+SELECT * FROM Customers WHERE Email='atapley2j@kinetecoinc.com'
+INSERT INTO CustomerResponses (CustomerID,PeopleCount)
+VALUES (92,3)
